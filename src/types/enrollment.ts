59281b24cee24f5ec44554052831318d53c39ac3
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-10-08 18:57:47 +0700

    Implement Approve and Reject Enrollment FE

--- src/types/enrollment.ts
+++ src/types/enrollment.ts
@@ -17,3 +17,97 @@ export interface RegisterEnrollmentDto {
     motherIdCard: string;
     motherJob: string;
 }
+
+interface EnrollmentFile {
+    _id: string;
+    length: number;
+    chunkSize: number;
+    uploadDate: string;
+    filename: string;
+}
+
+
+export interface EnrollmentListItem {
+    _id: string;
+    enrollmentCode: string;
+    studentName: string;
+    studentDob: string;
+    studentGender: "Nam" | "Nữ" | "Khác";
+    studentIdCard: string;
+    studentNation: string;
+    studentReligion: string;
+    address: string;
+    fatherName: string;
+    fatherPhoneNumber: string;
+    fatherEmail: string;
+    fatherIdCard: string;
+    fatherJob: string;
+    motherName: string;
+    motherPhoneNumber: string;
+    motherEmail: string;
+    motherIdCard: string;
+    motherJob: string;
+    state: string;
+    active: boolean;
+    createdAt: string;
+    updatedAt: string;
+    birthCertId: string;
+    healthCertId: string;
+    birthCertFiles: EnrollmentFile;
+    healthCertFiles: EnrollmentFile;
+}
+
+export interface EnrollmentsListResponse {
+    data: EnrollmentListItem[];
+    page: {
+        totalCount: number;
+        limit: number;
+        page: number;
+    };
+}
+
+export interface UploadPDFResponse {
+    message: string;
+    fileId: string;
+    fileName: string;
+}
+
+export interface ApproveEnrollmentDto {
+    _id: string;
+    birthCertId: string;
+    heathCertId: string;
+    approvedBy: string;
+}
+
+export interface UpdateEnrollmentDto {
+    _id: string;
+    enrollmentCode: string;
+    studentName: string;
+    studentDob: string;
+    studentGender: "Nam" | "Nữ" | "Khác";
+    studentIdCard: string;
+    studentNation: string;
+    studentReligion: string;
+    address: string;
+    fatherName: string;
+    fatherPhoneNumber: string;
+    fatherEmail: string;
+    fatherIdCard: string;
+    fatherJob: string;
+    motherName: string;
+    motherPhoneNumber: string;
+    motherEmail: string;
+    motherIdCard: string;
+    motherJob: string;
+    state: string;
+    active: boolean;
+    createdAt: string;
+    updatedAt: string;
+    birthCertId: string;
+    healthCertId: string;
+}
+
+export interface RejectEnrollmentDto {
+    _id: string;
+    reason: string;
+}
