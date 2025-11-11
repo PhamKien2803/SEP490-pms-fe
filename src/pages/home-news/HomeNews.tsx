3bfbe263a19fd13154336c3b1eeb4a044ec5a9ff
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-11-11 22:31:46 +0700

    config homenew redirect role

--- src/pages/home-news/HomeNews.tsx
+++ src/pages/home-news/HomeNews.tsx
@@ -1,10 +1,36 @@
+import { useMemo } from "react";
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import AdminNews from "./home-news-admin/AdminNews";
+import TeacherNews from "./home-news-teacher/TeacherNews";
+import ParentNews from "./home-news-parent/ParentNews";
+import { Button, Result } from "antd";
+import {
+    FrownOutlined
+} from "@ant-design/icons";
 
 function HomeNews() {
+    const user = useCurrentUser();
+
+    const isAdmin = user?.isAdmin;
+    const teacherId = useMemo(() => user?.staff, [user]);
+    const parentId = useMemo(() => user?.parent, [user]);
+
+    if (isAdmin) return <AdminNews />;
+    if (teacherId) return <TeacherNews />;
+    if (parentId) return <ParentNews />;
+
     return (
-        <div>
-            123
-        </div>
-    )
+        <Result
+            icon={<FrownOutlined />}
+            title="Không xác định vai trò người dùng"
+            subTitle="Vui lòng kiểm tra lại thông tin đăng nhập hoặc liên hệ quản trị viên."
+            extra={
+                <Button type="primary" onClick={() => window.location.reload()}>
+                    Tải lại trang
+                </Button>
+            }
+        />
+    );
 }
 
-export default HomeNews
+export default HomeNews;
