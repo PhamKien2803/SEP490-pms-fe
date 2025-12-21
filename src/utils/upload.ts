afcacff4cc14a32593494287a56fe826c137a276
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-12-22 01:03:51 +0700

    Update upload.ts

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -11,6 +11,25 @@ export const beforeUploadPDF = (file: RcFile) => {
     return isPdf || Upload.LIST_IGNORE;
 };
 
+// export const beforeUploadPDF = (file: RcFile) => {
+//     const isPdf = file.type === 'application/pdf';
+//     if (!isPdf) {
+//         toast.error('Bạn chỉ có thể tải lên file có định dạng .pdf!');
+//         return false;
+//     }
+
+//     // Regex kiểm tra tên file có dấu tiếng Việt
+//     const hasDiacriticalMarks = /[\u00C0-\u00FF\u0100-\u017F\u1EA0-\u1EFF\u1E00-\u1E7F\u0110\u1E02-\u1E3F]/.test(file.name);
+//     if (hasDiacriticalMarks) {
+//         toast.error('Tên file không được chứa dấu!');
+//         return false;
+//     }
+
+//     return true;
+// };
+
+
+
 export const beforeUploadImage = (file: RcFile) => {
     const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
     if (!isJpgOrPng) {
